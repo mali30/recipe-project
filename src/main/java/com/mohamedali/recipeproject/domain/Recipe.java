1fdcd09dec273aed06c17e7067eb513a6e81187c
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-10-17 12:45:22 -0400

    added ManyToMany for category. We also join the id columns

--- src/main/java/com/mohamedali/recipeproject/domain/Recipe.java
+++ src/main/java/com/mohamedali/recipeproject/domain/Recipe.java
@@ -1,8 +1,18 @@
 package com.mohamedali.recipeproject.domain;
 
-import jdk.internal.org.jline.utils.DiffHelper;
-
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Lob;
 
 import java.util.Set;
 
@@ -52,6 +62,18 @@ public class Recipe {
         mappedBy = "recipe"
     )
     private Set<Ingredient> ingredients;
+
+    /*
+        Specify a join table because if we don't it created two tables instead
+        categorY_recipe and recipe_category
+     */
+    @ManyToMany
+    @JoinTable(
+            name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id" )
+    )
+    private Set<Category> categories;
 
     public Set<Ingredient> getIngredients() {
         return ingredients;
@@ -148,6 +170,11 @@ public class Recipe {
         this.difficulty = difficulty;
     }
 
+    public Set<Category> getCategories() {
+        return categories;
+    }
 
-
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
